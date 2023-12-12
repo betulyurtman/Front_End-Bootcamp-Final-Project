1c9557d1294e5d544e20f28a3ccf164077d0064e
betulyurtman <betullyurtman@gmail.com>
2023-12-13 01:13:44 +0300

    Add: Dashboard Layout

--- book-app/src/pages/_app.js
+++ book-app/src/pages/_app.js
@@ -1,9 +1,12 @@
 import { Provider } from "react-redux"
 import store from "./store/store"
+import DashboardLayout from "@/components/DashboardLayout"
 
 export default function App({ Component, pageProps }) {
   return (
   <Provider store={store}>
+    <DashboardLayout>
       <Component {...pageProps} />
+    </DashboardLayout>
   </Provider>
 )};
