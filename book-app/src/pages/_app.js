f02b781f5cc6e6ea14b71849b0c23ae9e16d5567
betulyurtman <betullyurtman@gmail.com>
2023-12-14 21:38:19 +0300

    Add: Book Image Slider, Upload: react-slick, slick-carousel.

--- book-app/src/pages/_app.js
+++ book-app/src/pages/_app.js
@@ -1,6 +1,8 @@
 import { Provider } from "react-redux"
 import store from "../store/store"
 import DashboardLayout from "../components/DashboardLayout"
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function App({ Component, pageProps }) {
   return (
--- book-app/src/pages/index.js
+++ book-app/src/pages/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchBooks } from '../store/bookSlice';
-import { Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import BookCard from '../components/BookCard';
+import Slider from 'react-slick';
 
 const BookList = () => {
   const dispatch = useDispatch();
@@ -17,11 +18,61 @@ const BookList = () => {
   if (loadingBooks || loadingSearch) return <p>Loading...</p>;
   if (error) return <p>Error occurred: {error}</p>;
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false, 
+  };
+
   return (
     <div>
-      <Typography variant="h3" component="h1" gutterBottom>
+      {/* <Typography variant="h3" component="h1" gutterBottom>
         Self Improvement Books
-      </Typography>
+      </Typography> */}
+      <Box sx={{ width: '100%', overflow: 'hidden' }}>
+        <Slider {...settings}>
+          <Box sx={{ padding: 3 }}>
+            <img
+              src="https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Image 1"
+              style={{ width: '100%', maxWidth: 1000, height: 'auto', display: 'block', margin: 'auto' }}
+            />
+          </Box>
+          <Box sx={{ padding: 2 }}>
+            <img
+              src="https://images.pexels.com/photos/2099266/pexels-photo-2099266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Image 2"
+              style={{ width: '100%', maxWidth: 1000, height: 'auto', display: 'block', margin: 'auto' }}
+            />
+          </Box>
+          <Box sx={{ padding: 2 }}>
+            <img
+              src="https://images.pexels.com/photos/415078/pexels-photo-415078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Image 3"
+              style={{ width: '100%', maxWidth: 1000, height: 'auto', display: 'block', margin: 'auto' }}
+            />
+          </Box>
+          <Box sx={{ padding: 2 }}>
+            <img
+              src="https://images.pexels.com/photos/159597/book-read-literature-pages-159597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Image 4"
+              style={{ width: '100%', maxWidth: 1000, height: 'auto', display: 'block', margin: 'auto' }}
+            />
+          </Box>
+          <Box sx={{ padding: 2 }}>
+            <img
+              src="https://images.pexels.com/photos/2299028/pexels-photo-2299028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Image 5"
+              style={{ width: '100%', maxWidth: 1000, height: 'auto', display: 'block', margin: 'auto' }}
+            />
+          </Box>
+        </Slider>
+      </Box>
       <Typography variant="h6">
         Total Book Count: {displayBooks.length}
       </Typography>
