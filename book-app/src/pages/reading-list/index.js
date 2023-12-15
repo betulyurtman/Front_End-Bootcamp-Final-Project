20d5fd200863f52dee00cf58de4d24d52bfbc1b7
betulyurtman <betullyurtman@gmail.com>
2023-12-15 18:37:35 +0300

    Update: Reading List page using BookCard component.

--- book-app/src/pages/reading-list/index.js
+++ book-app/src/pages/reading-list/index.js
@@ -1,21 +1,9 @@
-import { Card, Typography, Grid, CardMedia, CardContent, Box, IconButton } from '@mui/material';
-import { Bookmark, BookmarkBorder } from '@mui/icons-material';
-import { useSelector, useDispatch } from 'react-redux';
-import { addToReadingList } from '../../store/readingListSlice';
+import { Typography, Grid } from '@mui/material';
+import { useSelector } from 'react-redux';
+import BookCard from '@/components/BookCard';
 
 const ReadingList = () => {
-    const dispatch = useDispatch();
     const readingList = useSelector((state) => state.readingList.readingList);
-
-    // For bookmark(adding to or removing from the reading list) button.
-    const handleToggleReadingList = (book) => {
-        dispatch(addToReadingList(book));
-      };
-
-    // Creating isBookInReadingList function that checks if a book with a given bookId is in the reading list. It returns a boolean.
-    const isBookInReadingList = (bookId) => {
-      return readingList.some((b) => b.id === bookId);
-    };
 
   return (
     <>
@@ -32,35 +20,9 @@ const ReadingList = () => {
       </Typography>
       <Grid container spacing={2}>
         {/* Displaying the books. */}
-        {readingList.map((book, index) => (
-          <Grid item xs={12} sm={6} md={4} key={book.id || `book-${index}`}>
-            <Card sx={{ position: 'relative', maxWidth: 345, '&:hover': { boxShadow: 6 }, height: '100%' }}>
-            <Box sx={{ position: 'absolute', top: 0, right: 0, zIndex: 1 }}>
-              <IconButton
-                color="inherit"
-                onClick={() => handleToggleReadingList(book)}
-              >
-                {isBookInReadingList(book.id) ? <Bookmark /> : <BookmarkBorder />}
-              </IconButton>
-            </Box>
-            <CardMedia
-              component="img"
-              image={book.thumbnail || '/default-book-thumbnail.jpg'}
-              alt={book.title}
-              sx={{ height: 500, objectFit: 'contain' }}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div" align="center">
-                {book.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary" align="center">
-                Author: {book.author}
-              </Typography>
-              <Typography variant="body2" color="text.secondary" align="center">
-                Page Count: {book.pageCount}
-              </Typography>
-            </CardContent>
-          </Card>
+        {readingList.map((book) => (
+          <Grid item xs={12} sm={6} md={4} key={book.id}>
+            <BookCard book={book} />
         </Grid>
       ))}
     </Grid>
